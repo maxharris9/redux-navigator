3f01640af9fa27e442bf99205f3deddc25df4197
Max Harris <max@nodesource.com>
2016-12-17 17:13:32 -0800

    force babel to target es5

--- redux-navigator.js
+++ redux-navigator.js
@@ -1,22 +1,24 @@
-const qs = require('qs');
-const url = require('url');
+'use strict';
 
-let _matcher;
-let _history;
-let _addressBarWrite;
+var qs = require('qs');
+var url = require('url');
+
+var _matcher = void 0;
+var _history = void 0;
+var _addressBarWrite = void 0;
 
 // if you ever want to kill this router listener,
 // hang onto the function it returns and then call it like this:
 // const unlisten = initRouter(dispatch, history, addressBarRead, addressBarWrite)
 // ... code flows, time elapses
 // unlisten()
-const initRouter = (dispatch, history, addressBarRead, addressBarWrite, inputUrl, inputAction) => {
+var initRouter = function initRouter(dispatch, history, addressBarRead, addressBarWrite, inputUrl, inputAction) {
   _history = history;
   _matcher = addressBarRead(dispatch);
   _addressBarWrite = addressBarWrite;
 
   listener(inputUrl);
-  return _history.listen((location, action) => {
+  return _history.listen(function (location, action) {
     if (!location || action !== 'POP') {
       return;
     }
@@ -29,21 +31,25 @@ const initRouter = (dispatch, history, addressBarRead, addressBarWrite, inputUrl
   });
 };
 
-const routerMiddleware = store => next => action => {
-  if (!action.invisibleToMiddleware) {
-    _addressBarWrite(_history, action);
-  }
+var routerMiddleware = function routerMiddleware(store) {
+  return function (next) {
+    return function (action) {
+      if (!action.invisibleToMiddleware) {
+        _addressBarWrite(_history, action);
+      }
 
-  return next(action);
+      return next(action);
+    };
+  };
 };
 
 // qs leaves us with output that looks like:
 // { thresholdSettings: 'true', license: 'false' }
 // but what we really want is something like:
 // { thresholdSettings: true, license: false }
-const sanitizeQuery = parsedQuery => {
-  let newQuery = {};
-  Object.keys(parsedQuery).forEach(key => {
+var sanitizeQuery = function sanitizeQuery(parsedQuery) {
+  var newQuery = {};
+  Object.keys(parsedQuery).forEach(function (key) {
     switch (parsedQuery[key].toLowerCase()) {
       case 'yes': // fall through intentionally
       case 'true':
@@ -63,18 +69,18 @@ const sanitizeQuery = parsedQuery => {
   return newQuery;
 };
 
-const listener = inputUrl => {
-  const parsedUrl = url.parse(inputUrl);
-  const match = _matcher.match(parsedUrl.pathname);
-  const parsedQuery = qs.parse(parsedUrl.query);
+var listener = function listener(inputUrl) {
+  var parsedUrl = url.parse(inputUrl);
+  var match = _matcher.match(parsedUrl.pathname);
+  var parsedQuery = qs.parse(parsedUrl.query);
   if (match) {
     match.fn(null, null, match, sanitizeQuery(parsedQuery));
   }
 };
 
-const mockHistory = {
-  push: (url, action) => {
-    return Object.assign({}, { url }, action);
+var mockHistory = {
+  push: function push(url, action) {
+    return Object.assign({}, { url: url }, action);
   }
 };
 
